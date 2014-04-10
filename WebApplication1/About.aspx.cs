43be21b94835d50b6df782e82fc37f8b0294ce3d
Gaurav Sharma <smp_dlh@yahoo.co.in>
2014-04-10 23:49:19 +0530

    first commit

---
+++ WebApplication1/About.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+            
+        }
+
+        
+        protected void Button2_Click1(object sender, EventArgs e)
+        {
+            string abc = TextBox1.Text;
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+            SqlCommand cmd = new SqlCommand("Select * from [dbo].[table4] where RegNo = '"+abc+"'", con);
+            con.Open();
+            SqlDataReader dr = null;
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    byte[] img = (byte[])dr["logo"];
+                    string base64string = Convert.ToBase64String(img, 0, img.Length);
+                            Label2.Text = (dr["RegNo"].ToString());
+                    Label7.Text = (dr["class"].ToString());
+                    Label8.Text = (dr["company"].ToString());
+                    Label3.Text = (dr["address"].ToString());
+                    Label1.Text = "<img src='data:image/png;base64," + base64string + "' alt='No Image' width='200px' vspace='5px' hspace='5px' />";
+                }
+            }
+            con.Close();
+        }
+    }
+}
